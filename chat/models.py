3e8fb0298fa8480469f0e7ccca45e525c1a34331
rafaelwnk <rafaelcarmo749@gmail.com>
2025-02-21 12:16:56 -0300

    configuração do models dos satélites

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Satellite(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    orbital_state = models.CharField(max_length=50, verbose_name='Estado Orbital')
+    designation = models.CharField(max_length=50, verbose_name='Designação')
+    orbit = models.CharField(max_length=50, verbose_name='Órbita')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Satélite'
+
+    def __str__(self):
+        return self.name
